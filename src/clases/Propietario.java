ff803d141f4c66eff90efad735a9d9d39d7c5c7f
Mariana Valladolid <lx05420@innovaccion.mx>
2022-12-07 20:13:49 -0600

    Encapsulamiento de atributos en clase propietario

--- src/clases/Propietario.java
+++ src/clases/Propietario.java
@@ -1,5 +1,79 @@
 package clases;
 
 public class Propietario {
+	//1. Propiedades -Declarar atributos
+	
+	public String Nombre;
+	public String RFC;
+	public String CURP;
+	public int Nacimiento;
+	public String Calle;
+	public String Colonia;
+	public String Ciudad;
+	public int CP;
 
-}
+	// 2. Constructor - Encapsulamiento de los atributos
+	public Propietario(String nombre, String rFC, String cURP, int nacimiento, String calle, String colonia,
+			String ciudad, int cP) {
+		super();
+		Nombre = nombre;
+		RFC = rFC;
+		CURP = cURP;
+		Nacimiento = nacimiento;
+		Calle = calle;
+		Colonia = colonia;
+		Ciudad = ciudad;
+		CP = cP;
+	}//constructor 1
+	
+	public Propietario(String nombre, String rFC, String cURP) {
+		super();
+		Nombre = nombre;
+		RFC = rFC;
+		CURP = cURP;
+	}// constructor2
+	
+	
+	// 3. Metodos 
+
+	public String getNombre() {
+		return Nombre;
+	}//getNombre
+
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}//setNombre
+
+	public String getRFC() {
+		return RFC;
+	}//getRFC
+
+	public void setRFC(String rFC) {
+		RFC = rFC;
+	}//setRFC
+
+	public String getCURP() {
+		return CURP;
+	}//getCURP
+
+	public void setCURP(String cURP) {
+		CURP = cURP;
+	}//setCURP
+
+	
+	//toString
+	@Override
+	public String toString() {
+		return "Propietario [Nombre=" + Nombre + ", RFC=" + RFC + ", CURP=" + CURP + ", Nacimiento=" + Nacimiento
+				+ ", Calle=" + Calle + ", Colonia=" + Colonia + ", Ciudad=" + Ciudad + ", CP=" + CP + "]";
+	}//toString
+	
+	
+
+	
+	
+	
+	
+	
+	
+}// Class Propietario
